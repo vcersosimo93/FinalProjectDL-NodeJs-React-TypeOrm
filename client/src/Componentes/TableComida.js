80f82d7b0c8481c1563102a783f0f2fd5385b3db
vicentec <vcersosimo93@gmail.com>
2023-02-04 11:25:19 -0300

    Arreglos Comidas

--- client/src/Componentes/TableComida.js
+++ client/src/Componentes/TableComida.js
@@ -21,6 +21,10 @@ const TableComida = ({ menu }) => {
     const descripcionMenu = menu.descripcion
     const esVegetarianoMenu = menu.esVegetariano
 
+    const [showErrorMenu, setShowErrorMenu] = useState(false);
+    const handleCloseErrorMenu = () => setShowErrorMenu(false);
+    const handleShowErrorMenu = () => setShowErrorMenu(true);
+
     const _onChangeVegetarianoUP = () => {
         try {
             ; (async () => {
@@ -33,6 +37,10 @@ const TableComida = ({ menu }) => {
         }
     }
 
+    const cerrarModalError = () => {
+        handleCloseErrorMenu();
+    }
+
     const handleSubmitUP = updateData => {
 
         updateData.preventDefault();
@@ -43,32 +51,37 @@ const TableComida = ({ menu }) => {
         let url = process.env.REACT_APP_LOCALHOST + '/menu/update'
         let method = 'PUT'
 
-        fetch(url, {
-            method: method,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "id": idMenu,
-                "esVegetariano": esVeget,
-                "descripcion": desc
+        if (desc.length > 0) {
+
+            fetch(url, {
+                method: method,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "id": idMenu,
+                    "esVegetariano": esVeget,
+                    "descripcion": desc
+                })
             })
-        })
-            .then(res => {
-                if (res.status !== 200 && res.status !== 201) {
-                    console.log(res)
-                    throw new Error('Fallo la modificación del menú.');
-                }
-                return res.json();
-            })
-            .then(resData => {
-                console.log(resData);
-                handleClose();
-            })
-            .catch(err => {
-                console.log(err);
-                alert("No se pudo modificar el menu seleccionado. Ya esta creada alguna opcion menu por fecha a publicar, o hay pedidos asociados a este menu. ");
-            });
+                .then(res => {
+                    if (res.status !== 200 && res.status !== 201) {
+                        console.log(res)
+                        throw new Error('Fallo la modificación del menú.');
+                    }
+                    return res.json();
+                })
+                .then(resData => {
+                    console.log(resData);
+                    handleClose();
+                })
+                .catch(err => {
+                    console.log(err);
+                    alert("No se pudo modificar el menu seleccionado. Ya esta creada alguna opcion menu por fecha a publicar, o hay pedidos asociados a este menu. ");
+                });
+        }else{
+            handleShowErrorMenu();
+        }
     }
 
 
@@ -108,79 +121,97 @@ const TableComida = ({ menu }) => {
     }
 
     const boolToString = (boolean) => {
-        if(boolean){
+        if (boolean) {
             return "Si"
         }
-        else{
+        else {
             return "No"
         }
     }
 
-  return (
-            <tr key={menu.id} >
-                <td >{menu.descripcion}</td>
-                <td >{boolToString(menu.esVegetariano)}</td>
-                <td ><Button variant="default" onClick={handleShowDL}><img src={Volver_img} className="iconosOtherOption" alt="volver" /></Button></td>
-                <td ><Button variant="default" onClick={handleShow}><img src={Lapiz_Comidas_Menu_img} className="iconosOtherOption" alt="modificar" /></Button></td>
+    return (
+        <tr key={menu.id} >
+            <td >{menu.descripcion}</td>
+            <td >{boolToString(menu.esVegetariano)}</td>
+            <td ><Button variant="default" onClick={handleShowDL}><img src={Volver_img} className="iconosOtherOption" alt="volver" /></Button></td>
+            <td ><Button variant="default" onClick={handleShow}><img src={Lapiz_Comidas_Menu_img} className="iconosOtherOption" alt="modificar" /></Button></td>
 
-                <Modal show={show} className="my-modal" onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Editar Menu</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form className="my-modal-form"  >
-                            <Form.Group className="mb-3" controlId="descripcion" >
-                                <Form.Label>Descripción</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    autoFocus
-                                    placeholder="prueba"
-                                    name="descripcion"
-                                    ref={descripcion}
-                                    defaultValue={descripcionMenu}
-                                />
-                            </Form.Group>
-                            <Form.Group
-                                className="mb-3"
-                                controlId="esVegetariano"
-                            >
-                                <Form.Check
-                                    type="checkbox"
-                                    label="Vegetariano"
-                                    name='esVegetariano'
-                                    ref={esVegetariano}
-                                    defaultChecked={esVegetarianoMenu}
-                                    onChange={_onChangeVegetarianoUP}
-                                />
-                            </Form.Group>
-                        </Form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button type="submit" variant="outline-primary" onClick={handleSubmitUP}  >
-                            Editar
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
+            <Modal show={show} className="my-modal" onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Editar Menu</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form className="my-modal-form"  >
+                        <Form.Group className="mb-3" controlId="descripcion" >
+                            <Form.Label>Descripción</Form.Label>
+                            <Form.Control
+                                type="text"
+                                autoFocus
+                                placeholder="prueba"
+                                name="descripcion"
+                                ref={descripcion}
+                                defaultValue={descripcionMenu}
+                            />
+                        </Form.Group>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="esVegetariano"
+                        >
+                            <Form.Check
+                                type="checkbox"
+                                label="Vegetariano"
+                                name='esVegetariano'
+                                ref={esVegetariano}
+                                defaultChecked={esVegetarianoMenu}
+                                onChange={_onChangeVegetarianoUP}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button type="submit" variant="outline-primary" onClick={handleSubmitUP}  >
+                        Editar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
-                <Modal show={showDL} className="my-modal" onHide={handleCloseDL}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Borrar Menú</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form className="my-modal-form"  >
-                            <Form.Group className="mb-3" controlId="Confirmar Borrado" >
-                                <Form.Label>Confirma borrar el menú seleccionado?</Form.Label>
-                            </Form.Group>
-                        </Form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button type="submit" variant="outline-primary" onClick={handleSubmitDL}  >
-                            Confirmar
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            </tr>
-        )
-    }
+            <Modal show={showDL} className="my-modal" onHide={handleCloseDL}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Borrar Menú</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form className="my-modal-form"  >
+                        <Form.Group className="mb-3" controlId="Confirmar Borrado" >
+                            <Form.Label>Confirma borrar el menú seleccionado?</Form.Label>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button type="submit" variant="outline-primary" onClick={handleSubmitDL}  >
+                        Confirmar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
-    export default TableComida
+            <Modal show={showErrorMenu} className="my-modal" onHide={handleCloseErrorMenu}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Error</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form className="my-modal-form"  >
+                        <Form.Group className="mb-3" controlId="Fecha no valida." >
+                            <Form.Label>La descripcion no puede estar vacia.</Form.Label>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button type="submit" variant="outline-primary" onClick={cerrarModalError}  >
+                        Ok
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </tr>
+    )
+}
+
+export default TableComida
